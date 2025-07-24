a1267138d76eb6370c56e2fa3229fdb07bdb543f
孙旭哲 <2313669@mail.nankai.edu.cn>
2025-07-24 14:58:34 +0800

    Delete MBTI_Multi-Agent_Debate_System/MBTI_Debate/MBTI_Multi-Agent_Debate_System directory

--- MBTI_Multi-Agent_Debate_System/MBTI_Debate/MBTI_Multi-Agent_Debate_System/MBTI_Advice/utils/mbti_prompts.py
+++
@@ -1,6 +0,0 @@
-from ..config.settings import get_prompt_template
-
-def get_prompt_for_mbti(mbti_type: str, user_query: str) -> str:
-    """获取特定MBTI类型的提示词"""
-    template = get_prompt_template(mbti_type)
-    return template.format(user_query=user_query)
