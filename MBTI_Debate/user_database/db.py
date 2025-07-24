e013705d617f7a99935c6eb858fd341b822604f9
孙旭哲 <2313669@mail.nankai.edu.cn>
2025-07-24 14:57:56 +0800

    Delete MBTI_Debate directory

--- MBTI_Debate/user_database/db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-
-# 替换为你的MySQL连接信息
-DATABASE_URL = "mysql+pymysql://root:051212@localhost:3306/dbabc?charset=utf8mb4"
-
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
